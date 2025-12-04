ab8e3f02f5628d0e7f279ccb609fbbd3a6f583d1
matylakontakt-glitch <matyla.kontakt@gmail.com>
2025-12-04 13:09:05 +0100

    update: zmiana portu na 5000 dla szybszego startu Render

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def chat():
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000)),
+        port=int(os.environ.get("PORT", 5000)),
         debug=False
     )
 
